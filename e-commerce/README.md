77f7e17dc7e89a707d817306e5b28b48cc32321c
raj-nada <rajnada1434@gmail.com>
2020-09-16 12:40:05 +0530

    first commit

---
+++ e-commerce/README.md
@@ -0,0 +1,3 @@
+# React-shopping Cart
+
+# step 1 :- create-react-app
---
+++ e-commerce/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ProductList from './components/ProductList';
+
+function App() {
+  return (
+    <div className='grid-container'>
+      <header>
+        <a href='/'>React Shoping Cart</a>
+        <div>Admin</div>
+      </header>
+      <main>
+        <ProductList />
+      </main>
+      <footer>All right is reserved</footer>
+    </div>
+  );
+}
+
+export default App;
