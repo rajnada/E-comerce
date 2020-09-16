9fea81513921d88818fffca8eb7b0373758bcb10
raj-nada <rajnada1434@gmail.com>
2020-09-16 19:15:25 +0530

    create filter component

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -3,11 +3,55 @@ import data from './data.json';
 //feature 1
 
 import Products from './components/Products';
+import FIlter from './components/FIlter';
 
 const App = () => {
-  const [products, setproducts] = useState(data.products);
-  const [size, setsize] = useState('');
-  const [sort, setsort] = useState('');
+  const [state, setState] = useState({
+    products: data.products,
+    size: '',
+    sort: '',
+  });
+
+  const sortProducts = (e) => {
+    // console.log(e.target.value);
+    const sort = e.target.value;
+    setState({
+      sort: sort,
+      products: state.products
+        .slice()
+        .sort((a, b) =>
+          sort === 'lowest'
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === 'highest'
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id > b._id
+            ? 1
+            : -1
+        ),
+    });
+  };
+
+  const filterProducts = (e) => {
+    console.log(e.target.value);
+    if (e.target.value === '') {
+      setState({
+        size: e.target.value,
+        product: data.products,
+      });
+    } else {
+      setState({
+        size: e.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(e.target.value) >= 0
+        ),
+      });
+    }
+  };
+
   return (
     <div className='grid-container'>
       <header>
@@ -17,7 +61,14 @@ const App = () => {
       <main>
         <div className='content'>
           <div className='main'>
-            <Products product={products}></Products>
+            <FIlter
+              count={state.products.length}
+              size={state.size}
+              sort={state.sort}
+              filterProducts={filterProducts}
+              sortProducts={sortProducts}
+            ></FIlter>
+            <Products product={state.products}></Products>
           </div>
           <div className='sidebar'></div>
         </div>
